18551cdf68c002cc7edd2f8ac1e825f853f9b4f4
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-22 16:10:05 -0500

    commit tree cc5fb8793df6c19a5bf198affe73fd5698553af3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers,string) {
+    let answer = drivers.filter(el => el === string)
+    return answer
+}
+   
+function fuzzyMatch(drivers, string) {
+    return drivers
+      .map((driver) => driver.split())
+      .filter((splitDriver) => splitDriver[0].indexOf(string) === string);
+  }
+
+//   function matchName(driver, string) {
+//       let result = 
+//   }
